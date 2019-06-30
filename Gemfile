27716fb6ca518b04ae66a0466558388425dfa696
matteobiffoni <biffoni.1700889@studenti.uniroma1.it>
2019-06-30 19:20:34 +0200

    Added OAuth for Facebook and added translations

--- Gemfile
+++ Gemfile
@@ -8,6 +8,8 @@ gem 'cancancan'
 gem 'rolify'
 gem 'bootstrap-sass'
 gem 'jquery-rails'
+gem 'omniauth-facebook'
+gem 'rails-i18n'
 #END
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.3'
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable
 
   validates_presence_of :user_role
   validates_presence_of :name
@@ -17,6 +18,38 @@ class User < ApplicationRecord
   belongs_to :publisher, class_name: "User", optional: true
   after_create :add_user_role
 
+
+  def self.from_omniauth(auth)
+    where(auth.slice(:provider, :uid)).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.name = auth.info.name
+      user.email = auth.info.email
+    end
+  end
+
+  def self.new_with_session(params, session)
+    if session["devise.user_attributes"]
+      new(session["devise.user_attributes"]) do |user|
+        user.attributes = params
+        user.valid?
+      end
+    else
+      super
+    end
+  end
+
+  def password_required?
+    super && provider.blank?
+  end
+
+  def update_with_password(params, *options)
+    if encrypted_password.blank?
+      update_attributes(params, *options)
+    else
+      super
+    end
+  end
 
   private
     def add_user_role
